ce14295d26e9978d8b909c0c1588f0d5a0aeca63
Santiago Venegas <esvv08@gmail.com>
2024-02-09 19:11:21 -0600

    first commit

---
+++ src/components/Searcher.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Search.css'
+
+export default function Searcher() {
+  return (
+    <div className='search'>
+      <h3> Buscador </h3>
+      <input type='text' />
+      <button> Buscar </button>
+    </div>
+  )
+}
+ 
